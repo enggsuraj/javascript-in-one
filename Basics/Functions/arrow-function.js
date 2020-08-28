22892fb9d4f6f50017e98ed8cc0542074339de19
Suraj <suraj.gehlot92@gmail.com>
2020-08-28 12:46:55 +0530

    update

--- Basics/Functions/arrow-function.js
+++ Basics/Functions/arrow-function.js
@@ -1,9 +1,29 @@
+//1.
+let sum = (a, b) => {
+  return a + b;
+};
+
+let result = sum(1, 5); //6
+console.log(result);
+
+//2.
+let sum2 = (a, b) => a + b;
+console.log(sum2(5, 6)); //11
+
+//3.
+let square = (x) => console.log(`Square: ${x * x}`);
+square(10); //Square: 100
+
+//4.
 const printName = (name) => {
   console.log(`Hello ${name}`);
 };
-printName("blogtheorem");
+printName("blogtheorem"); //Hello blogtheorem
 
-//CALLBACK
-document.querySelector("add_btn").addEventListener("click", () => {
-  alert("Button clicked!!");
-});
+//5.
+let setColor = (color) => {
+  return { value: color };
+};
+
+let backgroundColor = setColor("Red");
+console.log(backgroundColor.value); // "Red"
