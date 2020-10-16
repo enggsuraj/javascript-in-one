a5bb72999144eae3e9901123436dfb9e670e5fc2
Suraj <suraj.gehlot92@gmail.com>
2020-10-16 18:44:15 +0530

    javascript DOM

---
+++ DOM/dom.js
@@ -0,0 +1,55 @@
+/*
+Finding HTML Elements
+
+getElementById
+getElementByClassName
+getElementsByTagName
+
+
+document.querySelector()
+document.querySelectorAll()
+
+createElement
+createTextNode
+addClassList
+appendChild
+removeChild
+replace
+write
+
+Changing HTML Elements
+
+innerHTML
+setAttribute
+
+DOM Events
+
+onclick
+
+
+textContent
+innerText
+onload/onunload 
+onchange 
+onmouseover/ onmouseout 
+
+addEventListener()
+removeEventListener()
+
+Navigating Between Nodes
+
+node.childNodes
+node.firstChild
+node.lastChild
+node.parentNode
+node.nextSibling
+node.previousSibling
+
+nodeValue
+nodeType
+nodeName
+innerHTML
+
+
+
+*/ 
