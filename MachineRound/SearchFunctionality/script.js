463d00f6845e1435c35425d383b184110627b946
Suraj <info.blogtheorem@gmail.com>
2022-07-21 17:25:40 +0530

    MR:search and filter

--- MachineRound/SearchFunctionality/script.js
+++ MachineRound/SearchFunctionality/script.js
@@ -1,53 +1,96 @@
-// https://api.github.com/users
+// https://fakestoreapi.com/products
 
 /* 
 
 X Fetch API
 X Display
-X Search fn (filter) and display - full search or while searching 
+X Search fn (filter) and display - full search & while searching 
 
 */
 
 const container = document.querySelector(".container");
-const URL = "https://api.github.com/users";
+const URL = "https://fakestoreapi.com/products";
 const arr = [];
+
+// SEARCH
+let inputValue = document.getElementById("inputValue");
+let btn = document.getElementById("btn");
+let mensclothing_btn = document.getElementById("mensclothing");
+let electronics_btn = document.getElementById("electronics");
+let jewelery_btn = document.getElementById("jewelery");
+
+// FETCHDATA
+{
+  if (arr.length === 0) {
+    container.innerHTML = "Loading...";
+  }
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => displayData(data));
+}
 
 // DISPLAY DATA
 const displayData = (data) => {
   arr.push(data);
-  console.log(data);
   container.innerHTML = "";
+  if (data.length === 0) {
+    container.innerHTML = "No Item :(";
+  }
 
   data.forEach((each) => {
     let box = document.createElement("div");
     box.classList.add("box");
     box.innerHTML = `
-    <img src=${each.avatar_url} alt="">
-    <p id="title">${each.login}</p>`;
+    <img src=${each.image} alt="">
+    <p id="title">${each.title}</p>`;
     container.appendChild(box);
   });
 };
 
-// FETCHDATA
-const getMovieData = (URL) => {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((data) => displayData(data));
-};
-
-getMovieData(URL);
-
-// SEARCH
-let inputValue = document.getElementById("inputValue");
-let btn = document.getElementById("btn");
-
+// FILTER DATA
 const filterData = (data) => {
-  console.log(data, arr);
-  let finalArr = arr[0].filter((each) => each.login.includes(data));
-  console.log(finalArr);
+  let finalArr = arr[0].filter((each) =>
+    each.title.toLowerCase().includes(data)
+  );
   displayData(finalArr);
 };
 
-btn.addEventListener("click", (e) => {
+btn.addEventListener("click", () => {
   filterData(inputValue.value);
+});
+
+// MENS CLOTHING
+const filterDataClothing = (data) => {
+  let finalArr = arr[0].filter((each) =>
+    each.category.toLowerCase().includes(data)
+  );
+  displayData(finalArr);
+};
+
+mensclothing_btn.addEventListener("click", () => {
+  filterDataClothing(`men's clothing`);
+});
+
+// ELECTRONICS
+const filterDataElectronics = (data) => {
+  let finalArr = arr[0].filter((each) =>
+    each.category.toLowerCase().includes(data)
+  );
+  displayData(finalArr);
+};
+
+electronics_btn.addEventListener("click", () => {
+  filterDataElectronics(`electronics`);
+});
+
+// ELECTRONICS
+const filterDataJewelery = (data) => {
+  let finalArr = arr[0].filter((each) =>
+    each.category.toLowerCase().includes(data)
+  );
+  displayData(finalArr);
+};
+
+jewelery_btn.addEventListener("click", () => {
+  filterDataJewelery(`jewelery`);
 });
